da1bfd5b23c8232deb947120cfb92eabb3735d43
davidan90 <david16an@gmail.com>
2018-01-17 10:46:31 +0100

    add about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from'react';
+
+import Title from '../components/atoms/Title'
+
+const AboutPage = () => (
+    <main>
+        <Title text="I'm in About page" />
+    </main>
+)
+
+export default AboutPage
